c9afa1d2e8413b79c09abf4be6f19adb3b04e445
Ricardo Serrano <rxsh964@gmail.com>
2020-03-07 23:50:16 -0500

    push and pull chat messages

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,9 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatefulWidget {
-
   static String id = 'chat_sreen';
 
   @override
@@ -11,18 +11,35 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
+  final _firestore = Firestore.instance;
   final _auth = FirebaseAuth.instance;
-  FirebaseUser logInUser;
-  void getCurrentUser() async{
-    try{
+  FirebaseUser loggedInUser;
+  String messageText;
+
+  void getCurrentUser() async {
+    try {
       final user = await _auth.currentUser();
-      if(user != null){
-        logInUser = user;
-        print(logInUser.email);
+      if (user != null) {
+        loggedInUser = user;
+        print(loggedInUser.email);
       }
-    } catch(e){
+    } catch (e) {
       print(e);
+    }
+  }
+
+//  void getMessages () async {
+//    final messages = await _firestore.collection('messages').getDocuments();
+//    for (var message in messages.documents){
+//      print(message.data);
+//    }
+//  }
+
+  void messagesStream() async {
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+      for (var message in snapshot.documents) {
+        print(message.data);
+      }
     }
   }
 
@@ -41,8 +58,10 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
+                messagesStream();
+                //getMessages();
+                //_auth.signOut();
+                //Navigator.pop(context);
               }),
         ],
         title: Text('⚡️Chat'),
@@ -53,6 +72,30 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>(
+              stream: _firestore.collection('messages').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(
+                    child: CircularProgressIndicator(
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
+                  );
+                }
+                final messages = snapshot.data.documents;
+                List<Text> messageWidgets = [];
+                for (var message in messages) {
+                  final messageText = message.data['text'];
+                  final messageSender = message.data['sender'];
+                  final messageWidget =
+                      Text('$messageText from $messageSender');
+                  messageWidgets.add(messageWidget);
+                }
+                return Column(
+                  children: messageWidgets,
+                );
+              },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -61,14 +104,15 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _firestore.collection('messages').add(
+                          {'text': messageText, 'sender': loggedInUser.email});
                     },
                     child: Text(
                       'Send',
